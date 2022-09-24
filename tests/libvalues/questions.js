f619042957d686a214787ef7a0f94c033e854e47
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2022-09-24 17:09:25 -0400

    Update questions.js

--- tests/libvalues/questions.js
+++ tests/libvalues/questions.js
@@ -1,123 +1,123 @@
 questions = 
     [[//Anarchy-State`
-        [//2
+        [//4
             "The very existence of the state is a threat to our liberty.",
             "People's freedoms should be limitless.",
             "There should be no police force."
         ],
-        [//1
+        [//2
             "All forms of authority must be questioned.",
             "It would be best if the government only provided emergency services.",
             "The state should not be involved in the day-to-day lives of citizens."
         ],
-        [//-1
+        [//-2
             "It is important to maintain law and order.",
             "Some forms of hierarchy are justified.",
             "The state is best equipped to fight crime."
         ],
-        [//-2
+        [//-4
             "Anarchism has never, and will never, work.",
             "A lack of government will inevitably lead to violence and crime.",
             "We should be willing to sacrifice some liberties for the good of the nation."
         ]],[//Socialism-Capitalism`
-        [//2
+        [//4
             "From each according to his ability, to each according to his needs.",
             "The workers should control the means of production.",
             "We should prioritize both equality of opportunity and equality of outcome."
         ],
-        [//1
+        [//2
             "Basic necessities, such as healthcare, food, and water, are human rights.",
             "Our welfare system should be expanded to better provide for the people's needs.",
             "The wealthy have too much."
         ],
-        [//-1
+        [//-2
             "The freer the market, the freer the people.",
-            "<i>Laissez-faire</i> capitalism is the best economic system.",
+            "Laissez-faire capitalism is the best economic system.",
             "The poor chose to be that way."
         ],
-        [//-2
+        [//-4
             "He who does not work, neither shall he eat.",
             "Corporations should be treated as people.",
             "Socialism has never, and would never, work in practice."
         ]],[//World-Nation`
-        [//2
+        [//4
             "A united world government would be beneficial to mankind.",
             "We are first and foremost citizens of Earth.",
             "All borders should be opened or abolished."
         ],
-        [//1
+        [//2
             "The United Nations needs more power.",
             "Governments should be accountable to the international community.",
             "Overall, war is a net negative."
         ],
-        [//-1
+        [//-2
             "It is important to maintain our national sovereignty.",
             "My country is better than most others.",
             "I am proud to be in my country."
         ],
-        [//-2
+        [//-4
             "My nation should not be involved in international affairs.",
             "The United Nations should be abolished.",
             "Nobody in other countries has our best interests in mind"
         ]],[//Collective-Individual`
-        [//2
+        [//4
             "The individual has no power to make change, only the community does.",
             "Individuals have no value on their own.",
             "All property should be shared by the community at large."
         ],
-        [//1
+        [//2
             "Individuals should prioritize others over themselves.",
             "Individuals should primarily see themselves as members of their larger community.",
             "Selflessness is a virtue."
         ],
-        [//-1
+        [//-2
             "People should not be prevented from acting in their own self-interest.",
             "The individual has worth independent of their role in society.",
             "Organized actions are inferior to mass spontaneous actions for making social change."
         ],
-        [//-2
+        [//-4
             "All people are selfish by nature.",
             "No one else can tell me what to do.",
             "Morality is entirely subjective."
         
         ]],[//Progressive-Traditional`
-        [//2
+        [//4
             "All drugs should be legal.",
             "Transhumanism should be one of our primary goals.",
             "We should accelerate our technological and social progress.",
             "Traditions are nothing but a hindrance in modern society."
         ],
-        [//1
+        [//2
             "Same-sex marriage should be legal.",
             "Cannabis should be legal for recreational use.",
             "Traditions have no value on their own.",
             "We should develop more technologies to improve our lives."
         ],
-        [//-1
+        [//-2
             "To chase progress at all costs is dangerous.",
             "Maintaining 'family values' is very important.",
             "Traditions are generally important.",
             "The right to bear arms shall never be infringed."
         ],
-        [//-2
+        [//-4
             "Traditional medicines are often more effective than modern medicines.",
             "We should undo the technological and social progress we have made.",
             "We must go back to a basic way of life without modern conveniences.",
             "The Industrial Revolution and its consequences have been a disaster for the human race."
         ]],[//LibUnity-Disunity
-        [//2
+        [//8
             "All libertarians, left and right, should unite against authoritarianism.",
             "All forms of authoritarianism must be treated as equally evil."
         ],
-        [//1
+        [//4
             "Governmental issues are more important than economic or cultural issues.",
             "I would never want to live in a totalitarian state, regardless of its values."
         ],
-        [//-1
+        [//-4
             "I prefer left unity or right unity over libertarian unity.",
             "Right-libertarians and left-libertarians are too different to work together."
         ],
-        [//-2
+        [//-8
             "Libertarians who do not share my economic beliefs are not to be trusted.",
             "There is only one 'true' way to be a libertarian."
         ]
