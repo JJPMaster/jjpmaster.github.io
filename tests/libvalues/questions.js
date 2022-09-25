1a96c8df08a168c29621b3d2391a0218ae3a0f69
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2022-09-24 21:23:56 -0400

    add more questions

--- tests/libvalues/questions.js
+++ tests/libvalues/questions.js
@@ -1,39 +1,79 @@
 questions = 
     [[//Anarchy-State`
-        [//95
+        [//4
             "The very existence of the state is a threat to our liberty.",
             "People's freedoms should be limitless.",
             "There should be no police force."
         ],
-        [//2
-            "All forms of authority must be questioned.",
+        [//3
             "It would be best if the government only provided emergency services.",
-            "The state should not be involved in the day-to-day lives of citizens."
+            "A leader is not necessary to keep a society in good working order.",
+            "Direct democracy is superior to representative democracy."
+        ],
+        [//2
+            "All authority must be questioned.",
+            "The state does not tend to have the people's best interests in mind.",
+            "The state's power should be greatly limited, if not completely eliminated."
+        ],
+        [//1
+            "The police were created not to protect the people, but to uphold the status quo by force.",
+            "The state should not be involved in the day-to-day lives of citizens.",
+            "Government surveillance in any form is very dangerous to our liberty."
+        ],
+        [//-1
+            "No radical changes to our system of governance are necessary to ensure our liberty.",
+            "Absolute freedom makes a society dangerous",
+            "We should be willing to sacrifice some liberties for the good of the nation."
         ],
         [//-2
             "It is important to maintain law and order.",
             "Some forms of hierarchy are justified.",
             "The state is best equipped to fight crime."
         ],
+        [//-3
+            "The police should have more protection and power.",
+            "To achieve true liberty, the government does not necessarily need to decrease in power.",
+            "The state, in general, is a force for good, and should be maintained."
+        ],
         [//-4
             "Anarchism has never, and will never, work.",
             "A lack of government will inevitably lead to violence and crime.",
-            "We should be willing to sacrifice some liberties for the good of the nation."
+            "A central authority is essential to grant us rights."
         ]],[//Socialism-Capitalism`
         [//4
             "From each according to his ability, to each according to his needs.",
             "The workers should control the means of production.",
             "We should prioritize both equality of opportunity and equality of outcome."
         ],
+        [//3
+            "Oppression by corporations is more of a concern than oppression by governments.",
+            "Taxes should be increased on the rich to provide for the poor.",
+            "Public utilities like roads and electricity should be publicly owned."
+        ],
         [//2
             "Basic necessities, such as healthcare, food, and water, are human rights.",
             "Our welfare system should be expanded to better provide for the people's needs.",
-            "The wealthy have too much."
+            "The wealthy have far more than they need."
+        ],
+        [//1
+            "It is dangerous to leave the economy completely unregulated.",
+            "People should have equal opportunity to succeed.",
+            "People born to wealthy families have unfair success even when they aren't skilled and don't work hard."
+        ],
+        [//-1
+            "Inheritance is a legitimate form of wealth.",
+            "Regulation of the economy poses a risk to it.",
+            "Economies without any capitalism will collapse."
         ],
         [//-2
             "The freer the market, the freer the people.",
             "Laissez-faire capitalism is the best economic system.",
-            "The poor chose to be that way."
+            "Publicly owned industry is usually a bad idea."
+        ],
+        [//-3
+            "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
+            "People should have to work for anything they get.",
+            "The poor choose to be poor."
         ],
         [//-4
             "He who does not work, neither shall he eat.",
@@ -45,79 +85,135 @@ questions =
             "We are first and foremost citizens of Earth.",
             "All borders should be opened or abolished."
         ],
-        [//2
+        [//3
             "The United Nations needs more power.",
+            "Having independent nations with the risk of global conflict creates a large threat to humanity as a whole.",
+            "Most wars are fought for no legitimate reason."
+        ],
+        [//2
+            "I support regional unions, such as the European Union.",
             "Governments should be accountable to the international community.",
             "Overall, war is a net negative."
         ],
-        [//-2
+        [//1
+            "International trade is generally beneficial.",
+            "Alliances are important for protecting a nation",
+            "Nations should cooperate whenever it benefits them both"
+        ],
+        [//-1
             "It is important to maintain our national sovereignty.",
-            "My country is better than most others.",
+            "My nation is superior to many others.",
             "I am proud to be in my country."
         ],
-        [//-4
-            "My nation should not be involved in international affairs.",
+        [//-2
+            "International aid is a waste of money.",
+            "Alliances can pose a threat to a nation's sovereignty.",
+            "National cultures are important to protect."
+        ],
+        [//-3
+            "My nation should stay out of international affairs",
             "The United Nations should be abolished.",
-            "Nobody in other countries has our best interests in mind"
+            "Nobody in other countries has our best interests in mind."
+        ],
+        [//-4
+            "Diplomacy only hurts nations.",
+            "There should not be international law.",
+            "Any deals other countries want must be bad for us."
         ]],[//Collective-Individual`
         [//4
             "The individual has no power to make change, only the community does.",
             "Individuals have no value on their own.",
             "All property should be shared by the community at large."
         ],
+        [//3
+            "All individuals should be questioned, to make sure they are loyal to society.",
+            "There is a very clear distinction between 'good' and 'evil'.",
+            "Life has inherent meaning."
+        ],
         [//2
             "Individuals should prioritize others over themselves.",
-            "Individuals should primarily see themselves as members of their larger community.",
+            "Living a life of self-indulgence and luxury is not truly living.",
             "Selflessness is a virtue."
         ],
-        [//-2
-            "People should not be prevented from acting in their own self-interest.",
+        [//1
+            "Freedom of the community at large implies freedom of the individual.",
+            "Individuals should primarily see themselves as members of their larger community",
+            "An idea should not be rejected simply because it was socially constructed."
+        ],
+        [//-1
+            "People should organize in order to benefit their mutual self-interest.",
             "The individual has worth independent of their role in society.",
             "Organized actions are inferior to mass spontaneous actions for making social change."
         ],
+        [//-2
+            "People should not be prevented from acting in their own self-interest.",
+            "The non-aggression principle is a good guiding principle for ethics.",
+            "An ideal person should live primarily for their own pleasure."
+        ],
+        [//-3
+            "Any attempts to tell the individual what to do are forms of tyranny.",
+            "All systems of government are inherently oppressive.",
+            "All forms of hierarchy are illegitimate, as they prevent the individual from properly fulfilling their life."
+        ],
         [//-4
             "All people are selfish by nature.",
-            "No one else can tell me what to do.",
+            "It is impossible to truly be altruistic.",
             "Morality is entirely subjective."
         
         ]],[//Progressive-Traditional`
         [//4
             "All drugs should be legal.",
             "Transhumanism should be one of our primary goals.",
-            "We should accelerate our technological and social progress.",
-            "Traditions are nothing but a hindrance in modern society."
+            "We should accelerate our technological and social progress."
+        ],
+        [//3
+            "Churches should be taxed the same way other institutions are taxed.",
+            "Reason is more important than maintaining our culture.",
+            "Two consenting adults should be allowed to do whatever they want with each other, even if it makes me uncomfortable."
         ],
         [//2
             "Same-sex marriage should be legal.",
-            "Cannabis should be legal for recreational use.",
             "Traditions have no value on their own.",
-            "We should develop more technologies to improve our lives."
+            "It is important that we think long-term, well beyond our lifespans."
+        ],
+        [//1
+            "We should develop more technologies to improve our lives.",
+            "Scientific research should be publicly funded, to ensure that it can benefit us in the future.",
+            "Technological and social progress has benefitted society overall.",
+        ],
+        [//-1
+            "Prostitution should be illegal.",
+            "It is important that we maintain the traditions of our past.",
+            "To chase progress at all costs is dangerous.",
         ],
         [//-2
-            "To chase progress at all costs is dangerous.",
+            "Homosexuality is against my values.",
             "Maintaining 'family values' is very important.",
-            "Traditions are generally important.",
-            "The right to bear arms shall never be infringed."
+            "Traditions are generally important."
+        ],
+        [//-3
+            "The Industrial Revolution and its consequences have been a disaster for the human race.",
+            "Religion should play a role in government.",
+            "Society was much better many years ago than it is now."
         ],
         [//-4
             "Traditional medicines are often more effective than modern medicines.",
             "We should undo the technological and social progress we have made.",
-            "We must go back to a basic way of life without modern conveniences.",
-            "The Industrial Revolution and its consequences have been a disaster for the human race."
+            "We must go back to a basic way of life without modern conveniences."
         ]],[//LibUnity-Disunity
-        [//8
+        [//3
             "All libertarians, left and right, should unite against authoritarianism.",
             "All forms of authoritarianism must be treated as equally evil."
         ],
-        [//4
+        [//1
             "Governmental issues are more important than economic or cultural issues.",
             "I would never want to live in a totalitarian state, regardless of its values."
         ],
-        [//-4
+        [//-1
             "I prefer left unity or right unity over libertarian unity.",
             "Right-libertarians and left-libertarians are too different to work together."
         ],
-        [//-8
+        [//-3
             "Libertarians who do not share my economic beliefs are not to be trusted.",
             "There is only one 'true' way to be a libertarian."
         ]
